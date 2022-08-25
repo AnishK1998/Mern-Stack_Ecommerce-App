586513a248d7a430500aa94b2807cee5256ea991
anishhh98 <anish.kumar.singh@payomatix.com>
2022-08-26 00:21:24 +0530

    bugs fixed

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -5,7 +5,7 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="anishsingh.199804@gmail.com">
+      <a className="mailBtn" href="mailto:anishsingh.199804@gmail.com">
         <Button>Contact: anishsingh.199804@gmail.com</Button>
       </a>
     </div>
